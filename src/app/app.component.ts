34ab97ed97f4ba5d6e6e98a2d8778a3190861bef
Martin <trochezmartin@gmail.com>
2024-04-13 16:29:30 -0300

    componente grilla

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GrillaComponent } from './grilla/grilla.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, GrillaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/grilla/grilla.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grilla',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './grilla.component.html',
+  styleUrl: './grilla.component.css'
+})
+export class GrillaComponent {
+
+}
