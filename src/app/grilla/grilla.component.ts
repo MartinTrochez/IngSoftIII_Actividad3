9d1ccc9d4a121284e1debb75224381feeff51f3f
Martin <trochezmartin@gmail.com>
2024-04-13 17:02:15 -0300

    input

--- src/app/grilla/grilla.component.ts
+++ src/app/grilla/grilla.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ServicioService } from '../servicio.service';
 
 @Component({
   selector: 'app-grilla',
@@ -8,6 +9,29 @@ import { Component } from '@angular/core';
   templateUrl: './grilla.component.html',
   styleUrl: './grilla.component.css'
 })
-export class GrillaComponent {
+export class GrillaComponent implements OnInit {
+  posts: any[] = [];
+  searchText: string = '';
+  currentPage: number = 1;
+  pageSize: number = 10;
+
+  constructor(private servicioService: ServicioService) {
+  }
+
+  ngOnInit(): void {
+    this.loadPosts();
+  }
+
+  loadPosts(): void {
+    this.servicioService.getPosts().subscribe(posts => {
+      this.posts = posts;
+    });
+  }
+
+  filterPost(): void {
+    this.servicioService.filterPosts(this.searchText).subscribe(posts => {
+      this.posts = posts;
+    })
+  }
 
 }
---
+++ src/app/servicio.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServicioService {
+
+  constructor(private http: HttpClient) { }
+
+  getPosts(): Observable<any> {
+    return this.http.get(`https://jsonplaceholder.typicode.com/posts`)
+  }
+
+  filterPosts(searchTerm: string): Observable<any[]> {
+    return this.http.get<any[]>(`https://jsonplaceholder.typicode.com/posts?q=${searchTerm}`);
+  }
+}
